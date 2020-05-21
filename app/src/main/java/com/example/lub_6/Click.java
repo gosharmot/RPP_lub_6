ee5665b86cc6329a4ae8be4b380656a4712872d8
gosharmot <gosharmot@yandex.ru>
2020-05-21 19:26:57 +0300

    final

---
+++ app/src/main/java/com/example/lub_6/Click.java
@@ -0,0 +1,5 @@
+package com.example.lub_6;
+
+public interface Click {
+    public void click(int position);
+}
---
+++ app/src/main/java/com/example/lub_6/DataChangedListener.java
@@ -0,0 +1,5 @@
+package com.example.lub_6;
+
+public interface DataChangedListener {
+    public void notifyDataChanged();
+}
